b42fb2b80104d5d684bbcdb0d88b0556972f449d
couvq <quentincouvelaire@gmail.com>
2022-09-20 21:01:39 -0400

    learned functional component types and useState hooks with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+
+import { useState } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+
   return <div className="App">
     <span className="heading">Taskify</span>
+
+    <InputField />
   </div>;
 }
 
